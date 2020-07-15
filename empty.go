4a67a28eb736bda1b05825173631525e2ca6ae9a
fairyhunter13 <fairyhunter13@gmail.com>
2020-07-15 08:33:51 +0700

    Adding IsReflectZero to check if the reflect value is Zero type :sparkles:

--- empty.go
+++ empty.go
@@ -148,3 +148,27 @@ func IsTimeZero(t time.Time) bool {
 	return t.IsZero() || t.Format("2006-01-02 15:04:05") == ZeroTime0 ||
 		t.Format("2006-01-02 15:04:05") == ZeroTime1
 }
+
+// IsReflectZero check if the val of reflect.Value is a reflect.Zero of it's type.
+// This operation is solely based on reflect.Zero, not to check if the actual underlying value is zero.
+// To check if the value of reflect.Value is zero, use IsZero or IsValueZero.
+func IsReflectZero(val reflect.Value) (result bool) {
+	if !val.IsValid() {
+		return
+	}
+
+	zero := reflect.Zero(val.Type())
+	result = val == zero
+	return
+}
+
+// IsInterfaceReflectZero checks whether the interface is nil.
+// It also checks if the reflect.Value of val is reflect.Zero of it's type.
+func IsInterfaceReflectZero(val interface{}) (result bool) {
+	if val == nil {
+		result = true
+		return
+	}
+
+	return IsReflectZero(reflect.ValueOf(val))
+}
