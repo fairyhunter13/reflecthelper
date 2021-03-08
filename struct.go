0268f540944eac3794623f8867280f5df8098fff
fairyhunter13 <fairyhunter13@gmail.com>
2021-03-08 11:24:54 +0700

    Modifying API for flexibility :alien: :wheelchair:

--- struct.go
+++ struct.go
@@ -10,10 +10,19 @@ type IterateStructFunc func(structInput reflect.Value, field reflect.Value)
 // ReflectStruct is a custom struct for representing reflect.Struct.
 type ReflectStruct struct {
 	reflect.Value
+	kind reflect.Kind
+}
+
+func (s *ReflectStruct) isStruct() bool {
+	return IsKindStruct(s.kind)
 }
 
 // Iterate iterates all the field in this struct.
 func (s *ReflectStruct) Iterate(fns ...IterateStructFunc) *ReflectStruct {
+	if !s.isStruct() {
+		return s
+	}
+
 	// TODO: Add logic in here.
 	return s
 }
@@ -21,24 +30,24 @@ func (s *ReflectStruct) Iterate(fns ...IterateStructFunc) *ReflectStruct {
 // IteratePanic iterates all the field in this struct by returning err when the iteration function panics.
 func (s *ReflectStruct) IteratePanic(fns ...IterateStructFunc) (err error) {
 	defer RecoverFn(&err)
+	if !s.isStruct() {
+		return
+	}
 
 	// TODO: Add logic in here.
 	return
 }
 
 // CastStruct casts the val of reflect.Value to the ReflectStruct.
-func CastStruct(val reflect.Value) (res ReflectStruct, err error) {
+func CastStruct(val reflect.Value) (res ReflectStruct) {
 	val = GetChildElem(val)
-	err = getErrIsValid(val)
-	if err != nil {
-		return
-	}
 
-	switch GetKind(val) {
-	case reflect.Struct:
-		res = ReflectStruct{val}
-	default:
-		err = getErrUnimplementedCasting(val, reflect.Struct)
+	kind := GetKind(val)
+	if IsKindStruct(kind) {
+		res = ReflectStruct{
+			Value: val,
+			kind:  kind,
+		}
 	}
 	return
 }
--- struct_test.go
+++ struct_test.go
@@ -19,7 +19,6 @@ func TestCastStruct(t *testing.T) {
 		name     string
 		args     args
 		wantKind reflect.Kind
-		wantErr  bool
 	}{
 		{
 			name: "invalid nil value",
@@ -27,7 +26,6 @@ func TestCastStruct(t *testing.T) {
 				val: reflect.ValueOf(nil),
 			},
 			wantKind: reflect.Invalid,
-			wantErr:  true,
 		},
 		{
 			name: "invalid slice value",
@@ -35,7 +33,6 @@ func TestCastStruct(t *testing.T) {
 				val: reflect.ValueOf([]int{1, 2, 3}),
 			},
 			wantKind: reflect.Invalid,
-			wantErr:  true,
 		},
 		{
 			name: "valid struct value",
@@ -43,7 +40,6 @@ func TestCastStruct(t *testing.T) {
 				val: reflect.ValueOf(test{"Hi!"}),
 			},
 			wantKind: reflect.Struct,
-			wantErr:  false,
 		},
 		{
 			name: "valid ptr struct value",
@@ -51,16 +47,11 @@ func TestCastStruct(t *testing.T) {
 				val: reflect.ValueOf(&test{"Hi!"}),
 			},
 			wantKind: reflect.Struct,
-			wantErr:  false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotRes, err := CastStruct(tt.args.val)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("CastStruct() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
+			gotRes := CastStruct(tt.args.val)
 			assert.Equal(t, tt.wantKind, GetKind(gotRes.Value))
 		})
 	}
